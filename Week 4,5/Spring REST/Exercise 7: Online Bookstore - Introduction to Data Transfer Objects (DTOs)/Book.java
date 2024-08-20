a84633b9a10dd116524aa36a5c43e75ba9e6e69a
Soumyodip Sutradhar <74486329+Samsand-5@users.noreply.github.com>
2024-08-20 21:42:30 +0530

    Update Book.java

--- Week 4,5/Spring REST/Exercise 7: Online Bookstore - Introduction to Data Transfer Objects (DTOs)/Book.java
+++ Week 4,5/Spring REST/Exercise 7: Online Bookstore - Introduction to Data Transfer Objects (DTOs)/Book.java
@@ -14,4 +14,35 @@ public class Book {
     private Double price;
 
     // Getters and Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Double getPrice() {
+        return price;
+    }
+
+    public void setPrice(Double price) {
+        this.price = price;
+    }
 }
